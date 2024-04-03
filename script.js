eb282d191f23c4626c610492ea395d56f44722f7
lord20091 <abdulazizbozorov009@gmail.com>
2024-04-03 17:20:16 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let ism = prompt('Ism ?');
+
+let familiya = prompt('Familiya ?');
+
+let maktab = prompt('Maktab ?');
+
+let sinf = prompt('Sinf ?');
+
+let manzil = prompt('Manzil ?');
+
+
+let natija = "Salom" + ' ' + ism + ' ' + familiya + ' ' + maktab + ' '
++ sinf + ' ' + manzil
+
+console.log(natija);
